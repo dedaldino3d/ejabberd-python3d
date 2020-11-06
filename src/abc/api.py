2846573519634ea2df5e58d5b69fa17f1d7eb09f
Dedaldino3D <dedaldinoantonio7@gmail.com>
2020-11-06 12:45:54 +0100

    Add implementation of a base class for API

--- src/abc/api.py
+++ src/abc/api.py
@@ -88,3 +88,93 @@ class API(ABCMeta):
         :param argumentd: The dictionary containing the arguments
         """
         return response
+
+
+class EjabberdBaseAPI(ABCMeta):
+    @abstractmethod
+    def echo(self, sentence):
+        pass
+    
+    @abstractmethod
+    def registered_users(self, host):
+        pass
+    
+    @abstractmethod
+    def register(self, user, host, password):
+        pass
+    
+    @abstractmethod
+    def unregister(self, user, host):
+        pass
+    
+    @abstractmethod
+    def change_password(self, user, host, password, newpass):
+        pass
+    
+    @abstractmethod
+    def check_password_hash(self, user, host, password):
+        pass
+    
+    @abstractmethod
+    def set_nickname(sel, user, host, nickname):
+        pass
+    
+    @abstractmethod
+    def conneected_users(self):
+        pass
+    
+    @abstractmethod
+    def conneected_users_info(self):
+        pass
+    
+    @abstractmethod
+    def conneected_users_number(self):
+        pass
+    
+    @abstractmethod
+    def user_sessions_info(self):
+        pass
+    
+    @abstractmethod
+    def muc_online_rooms(self, host=None):
+        pass
+    
+    @abstractmethod
+    def muc_online_rooms(self. host=None):
+        pass
+
+    @abstractmethod
+    def create_room(self, name, service, host):
+        pass
+    
+    @abstractmethod
+    def destroy_room(self, name, service, host):
+        pass
+    
+    @abstractmethod
+    def get_room_options(self, name, service):
+        pass
+    
+    @abstractmethod
+    def change_room_option(self, name, service, option, value):
+        pass
+    
+    @abstractmethod
+    def set_room_affiliation(self, name, service, jid, affiliation):
+        pass
+    
+    @abstractmethod
+    def get_room_affiliations(self, name, service):
+        pass
+
+    @abstractmethod
+    def add_roster_item(self, localuser, localserver, user, server, nick, group, subs):
+        pass
+    
+    @abstractmethod
+    def remove_rosteritem(self, localuser, localserver, user, server):
+        pass
+    
+    @abstractmethod
+    def get_roster(self, user, host):
+        pass
