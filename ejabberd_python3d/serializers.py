b0e92a141f8b279d0946ce256a49abed4ad315be
Dedaldino3D <dedaldinoantonio7@gmail.com>
2020-11-13 23:04:01 +0100

    Refactor code (code cleanup)

--- ejabberd_python3d/serializers.py
+++ ejabberd_python3d/serializers.py
@@ -4,18 +4,16 @@ from six import string_types
 from .abc.api import Enum, APIArgumentSerializer
 
 
-
 class StringSerializer(APIArgumentSerializer):
     def to_api(self, value):
         if not isinstance(value, string_types):
             raise ValueError("Expects str or unicode, but got {}".format(type(value)))
         return value
-    
+
     def to_builtin(self, value):
         if not isinstance(value, string_types):
             raise ValueError("Expects str or unicode, but got {}".format(type(value)))
         return value
-
 
 
 class IntegerSerializer(APIArgumentSerializer):
@@ -23,10 +21,9 @@ class IntegerSerializer(APIArgumentSerializer):
         if not isinstance(value, int):
             raise ValueError("Expects int or long, but got {}".format(type(value)))
         return str(value)
-    
+
     def to_builtin(self, value):
         return int(value)
-
 
 
 class PositiveIntegerSerializer(IntegerSerializer):
@@ -34,7 +31,7 @@ class PositiveIntegerSerializer(IntegerSerializer):
         if not isinstance(value, int) or value < 0:
             raise ValueError("Expects positive int or long, but got {}".format(type(value)))
         return super(PositiveIntegerSerializer, self).to_api(value)
-    
+
     def to_builtin(self, value):
         res = super(PositiveIntegerSerializer, self).to_builtin(value)
         if res < 0:
@@ -42,18 +39,16 @@ class PositiveIntegerSerializer(IntegerSerializer):
         return res
 
 
-
 class BooleanSerializer(APIArgumentSerializer):
     def to_api(self, value):
         if not isinstance(value, bool):
             raise ValueError("Expects boolean")
-        return 'true' if value else 'false
-    
+        return 'true' if value else 'false'
+
     def to_builtin(self, value):
         if value not in ('true', 'false'):
             raise ValueError("Expects true|false, but got {}".format(type(value)))
         return value == 'true'
-
 
 
 class EnumSerializer(ABCMeta, StringSerializer):
@@ -61,7 +56,7 @@ class EnumSerializer(ABCMeta, StringSerializer):
     @property
     def enum_class(self):
         pass
-    
+
     def to_api(self, value):
         assert issubclass(self.enum_class, Enum)
         if isinstance(value, self.enum_class):
@@ -79,4 +74,3 @@ class EnumSerializer(ABCMeta, StringSerializer):
         res = self.enum_class.get_by_name(value)
         if res is None:
             raise ValueError("Expects enum value for {}, but got {}".format(self.enum_class, type(value)))
-        
